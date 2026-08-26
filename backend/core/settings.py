a855d0641d3b0d306c240abee6a0973fc6b17e8a
Eduardo Hernández Contreras <145513854+eduardoRuined@users.noreply.github.com>
2026-08-25 19:27:40 -0600

    fix: servir archivos de media en desarrollo

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'music',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,3 +138,7 @@ REST_FRAMEWORK={
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
+CORS_ALLOWED_ORIGINS=[
+    'http://localhost:5173','http://127.0.0.1:5173',
+]
